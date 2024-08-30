fdbfbad801d18bbaecffebbeb957c6c53320042b
sila7 <sila7@SILA>
2024-08-30 10:33:28 +0300

    Add project files.

---
+++ polymorphism/BaseGeometricShape .cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace polymorphism
+{
+
+
+    public class BaseGeometricShape
+    {
+        public double height {get; set;}
+        public double width { get; set; }
+
+        public virtual double CalculateArea(double height, double width)
+        {
+            double area = height * width;
+            return area;
+        }
+    }
+
+
+        public class Square: BaseGeometricShape
+        {
+          
+        }
+
+        public class Rectangle : BaseGeometricShape
+        {
+
+        }
+
+        public class RightTriangle : BaseGeometricShape
+        {
+
+        public override double CalculateArea(double height, double width)
+        {
+            double area = (height * width) / 2;
+            return area;
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿using polymorphism;
+
+BaseGeometricShape square1 = new Square();
+Console.WriteLine("Area of square1 :" + square1.CalculateArea(3,3));
+
+BaseGeometricShape rectangle1 = new Rectangle();
+Console.WriteLine("Area of rectangle1 :" + rectangle1.CalculateArea(3, 4));
+
+BaseGeometricShape right_triangle1 = new RightTriangle();
+Console.WriteLine("Area of right_triangle1 :" + right_triangle1.CalculateArea(6, 4));
